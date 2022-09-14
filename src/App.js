bd437ba0abadf0aeed158c9a5a24cab5a15a5cf9
Tal Ben-Zur <tbz1996@gmail.com>
2022-09-14 22:22:21 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import "./App.css";
+import Lottery from "./Lottery";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Lottery />
+        <Lottery title={"Mini Daily"} maxNum={10} numBalls={4} />
+      </div>
+    );
+  }
+}
+
+export default App;
